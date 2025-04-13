5fca0ec5c74cc8cfeb0c531b63313a4ca2d75379
BREEZE <157225495+BREEZE07-TG@users.noreply.github.com>
2025-04-13 08:24:19 +0530

    Update __init__.py

--- UB/__init__.py
+++ UB/__init__.py
@@ -7,7 +7,7 @@ FORMAT = "[UB]:%(message)s"
 logging.basicConfig(level=logging.INFO,handlers=[logging.FileHandler('logs.txt'),
                                                  logging.StreamHandler()],format=FORMAT)
 
-app = Client("userBot",
+app = Client(c.string_session,
              api_id=c.api_id,
              api_hash=c.api_hash,
              plugins=dict(root='module'))
